a07e2cb2db2e69c34a59c823ed4ff5834d06fa85
vazand <vazandvel@gmail.com>
2022-11-23 14:18:54 +0530

    sbt-sonatype-helloworld

---
+++ sbt-sonatype-helloworld/src/main/scala/example/Hello.scala
@@ -0,0 +1,11 @@
+package example
+
+object Hello extends  App {
+  println("Hello...!")
+  println("This package does nothing ")
+  println("But used to know how to create sbt-sonatype project :)")
+  println("If you don’t take risks, you can’t create a future. - Monkey D Luffy")
+
+}
+
+
